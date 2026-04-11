fdd579993ca147c432470ec230e71071d00b6394
saandeep veeramalla <saandeepveeramalla@saandeeps-MacBook-Pro.local>
2026-04-11 16:03:31 -0400

    1H: WebClient integration with cart-product validation

--- cart-service/src/main/java/com/example/cart_service/service/CartItemService.java
+++ cart-service/src/main/java/com/example/cart_service/service/CartItemService.java
@@ -1,8 +1,10 @@
 package com.example.cart_service.service;
 
+import com.example.cart_service.dto.ProductResponse;
 import com.example.cart_service.model.CartItem;
 import com.example.cart_service.repository.CartItemRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,24 +13,38 @@ import java.util.Optional;
 public class CartItemService {
 
     private final CartItemRepository cartItemRepository;
+    private final WebClient webClient;
 
-    public CartItemService(CartItemRepository cartItemRepository) {
+    public CartItemService(CartItemRepository cartItemRepository, WebClient webClient) {
         this.cartItemRepository = cartItemRepository;
+        this.webClient = webClient;
     }
 
     public CartItem createCartItem(CartItem cartItem) {
+
+        ProductResponse product = webClient.get()
+                .uri("http://localhost:8081/products/" + cartItem.getProductId())
+                .retrieve()
+                .bodyToMono(ProductResponse.class)
+                .block();
+
+        if (product == null) {
+            throw new RuntimeException("Product not found");
+        }
+
+        if (product.getStock() < cartItem.getQuantity()) {
+            throw new RuntimeException("Not enough stock");
+        }
+
         return cartItemRepository.save(cartItem);
     }
-
     public List<CartItem> getAllCartItems() {
         return cartItemRepository.findAll();
     }
-
-    public Optional<CartItem> getCartItemById(Integer id) {
-        return cartItemRepository.findById(id);
-    }
-
     public List<CartItem> getCartItemsByCartId(Integer cartId) {
         return cartItemRepository.findByCartId(cartId);
+    }
+    public Optional<CartItem> getCartItemById(Integer id) {
+        return cartItemRepository.findById(id);
     }
 }
